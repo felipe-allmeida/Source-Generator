b9e9342c0ad051020928dd1272fda5e93fb20a22
felipe-allmeida <felipe.allmeida.dev@gmail.com>
2020-05-31 05:34:00 -0300

    source generator

---
+++ TestSourceCodeGenerator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using AutoNotify;
+
+namespace TestSourceCodeGenerator
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var model = new ExampleViewModel();
+
+            model.PropertyChanged += (src, args) =>
+            {
+                Console.WriteLine($"A propriedade {args.PropertyName} foi alterada");
+            };
+
+            model.Text = "Felipe";
+        }
+    }
+
+    // The view model we'd like to augment
+    public partial class ExampleViewModel
+    {
+        [AutoNotify]
+        private string _text = "private field text";        
+
+        [AutoNotify(PropertyName = "Count")]
+        private int _amount = 5;
+    }
+
+}
